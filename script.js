ba5d2994a25b7c42d2824e9cff9d462e97ff0640
kamily_dev <matheusavakin844@gmail.com>
2025-07-25 03:17:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+const entrada = document.querySelector('.inputEntrada')
+const msgRes = document.querySelector('.content-tarefa');
+const btn = document.querySelector('.btn');
+const limparTarefa = document.querySelector('.btn-limpar');
+const teste = document.querySelector('.teste')
+const body = document.querySelector('body')
+btn.addEventListener('click' , function(){
+
+  
+    if(entrada.value !== ''){
+
+        const teste = document.createElement('div')
+        teste.innerHTML +=`
+
+
+        <p class='tarefa'>${entrada.value}</p> 
+        <div class ='buttons'>
+ 
+        <button class="btn-excluir">X</button>
+        <button class='tarefaConcluida'>✔</button></button>
+        </div>
+    
+        </div>
+   
+        
+        `
+        teste.classList.add('teste')
+
+        body.appendChild(teste)
+        const excluir = teste.querySelector('.btn-excluir')
+        const concluid = teste.querySelector('.tarefaConcluida')
+        const tarefaValue = teste.querySelector('.tarefa')
+          excluir.addEventListener('click',function(){
+          teste.remove()
+        })
+
+      
+        concluid.addEventListener('click',function(){
+         tarefaValue.style.textDecoration = 'line-through'
+         teste.style.backgroundColor = '#064e0996'
+        })
+
+    
+
+
+
+}});
+
+limparTarefa.addEventListener('click' , function(){
+   
+})
+
+
